d3639b0a32a17845a8ca62cbc5141154f928dc08
Migsar Navarro <migsar.navarro@gmail.com>
2019-01-05 15:47:21 -0600

    Adding typescript starter files.

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { FlNeur } from './fl-neur';
+
+import './App.css';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <FlNeur />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/fl-neur.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export class FlNeur extends React.Component {
+  public render() {
+    return (
+      <div>
+        <h1>Boo!</h1>
+      </div>
+    );
+  }
+}
+
